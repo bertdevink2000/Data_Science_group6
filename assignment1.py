7f59ba48ff329f0f2e7d45f02ab8d9f0a3ebc7dd
bertdevink2000 <bertdevinkheusden@gmail.com>
2023-03-12 19:45:22 +0100

    Lots of changes

--- assignment1.py
+++ assignment1.py
@@ -3,16 +3,16 @@ import json
 import geopandas as gpd
 import numpy as np
 import pandas as pd
-from bokeh.io import show, output_file, curdoc
-from bokeh.models import ColumnDataSource, ColorBar, HoverTool, DateSlider, RadioButtonGroup
+from bokeh.io import show, output_file
+from bokeh.models import ColumnDataSource, ColorBar, HoverTool, Slope
 from bokeh.models import GeoJSONDataSource, LinearColorMapper
 from bokeh.plotting import figure
 from bokeh.transform import dodge
-from bokeh.layouts import column
+from bokeh.layouts import column, gridplot
 import bokeh.palettes
 from dataprep.clean import clean_country
 from bokeh.palettes import brewer
-from datetime import date
+from sklearn.linear_model import LinearRegression
 import datetime
 
 dir = "assignment1_data/"
@@ -52,16 +52,18 @@ def import_csvs(name, suffix="", encoding="utf-16"):
 def filter_sales(inputdf):  # refunds compleet wegfilteren. Zowel voor transaction count als hoeveelheid geld handig
     temp = inputdf
     droplist = list()
-    refunds = temp.loc[(temp["Transaction Type"] == "Charge refund") | (temp["Transaction Type"] == "Refund"), "Description"]
+    refunds = temp.loc[
+        (temp["Transaction Type"] == "Charge refund") | (temp["Transaction Type"] == "Refund"), "Description"]
     for i in refunds:
-       index = temp[temp["Description"] == i].index
-       for j in index:
-           droplist.append(j)
+        index = temp[temp["Description"] == i].index
+        for j in index:
+            droplist.append(j)
 
     temp = temp.drop(droplist)
     temp = temp[temp["Transaction Type"].isin(["Charge", "Charged"])]
 
     return temp
+
 
 def count_transactions(inputdf):  # telt aantal transacties, gesplitst op Sku Id en totaal
     temp = inputdf[["Transaction Date", "Sku Id"]].value_counts(sort=False).copy()
@@ -110,7 +112,8 @@ data_sales = import_csvs(dir + "sales_" + year, encoding="utf-8")
 data_sales["Transaction Date"] = pd.to_datetime(data_sales["Transaction Date"])
 data_sales.pop("Base Plan ID")
 data_sales.pop("Offer ID")
-sales_ddfive = data_sales[data_sales["Product id"] == "com.vansteinengroentjes.apps.ddfive"]# andere apps eruit filteren
+sales_ddfive = data_sales[
+    data_sales["Product id"] == "com.vansteinengroentjes.apps.ddfive"]  # andere apps eruit filteren
 sales_norefunds = filter_sales(sales_ddfive)  # zonder refunds en zonder google fee
 sales_count = count_transactions(sales_norefunds)
 sales_count_weekperiods = convert_to_weekdays(sales_count)
@@ -143,7 +146,7 @@ sales_count_cds2 = ColumnDataSource(sales_count)
 sales_count_weekperiods_cds = ColumnDataSource(sales_count_weekperiods)
 
 # Sales Volume Figure
-output_file("visualization.html", title="Placeholder")
+output_file("visualization.html", title="Data Visualization")
 
 fig1 = figure(title="Transaction count over time", x_axis_type="datetime", width=1900, x_axis_label="Date",
               y_axis_label="Transaction count")
@@ -210,78 +213,147 @@ for country in unique_countries:
     prev_sales = 0
     for date in unique_dates:
         daily_sales = len(data_sales[(data_sales["Date"] == date) & (data_sales["Country"] == country)])
-        data_countries.loc[(data_countries["country_code"] == country) & (data_countries["Date"] == date), ["DailySales"]] = daily_sales
-        data_countries.loc[(data_countries["country_code"] == country) & (data_countries["Date"] == date), ["SalesOverTime"]] = daily_sales + prev_sales
+        data_countries.loc[(data_countries["country_code"] == country) & (data_countries["Date"] == date), [
+            "DailySales"]] = daily_sales
+        data_countries.loc[(data_countries["country_code"] == country) & (data_countries["Date"] == date), [
+            "SalesOverTime"]] = daily_sales + prev_sales
         prev_sales = prev_sales + daily_sales
 
 # Merging geopandas geometry and the data
-json_countries = set_date(datetime.date(2021, 12, 31))
+json_countries_start = set_date(datetime.date(2021, 6, 1))
+json_countries_end = set_date(datetime.date(2021, 12, 31))
 
 # Making the geopandas bokeh plot
-geosource_countries = GeoJSONDataSource(geojson=json_countries)
-#palette = brewer['YlGnBu'][8]
+geosource_countries_start = GeoJSONDataSource(geojson=json_countries_start)
+geosource_countries_end = GeoJSONDataSource(geojson=json_countries_end)
 palette = brewer['YlGn'][8]
 palette = palette[::-1]
-color_mapper = LinearColorMapper(palette=palette, low=0, high = 300)
+color_mapper_start = LinearColorMapper(palette=palette, nan_color='#d9d9d9')
+color_mapper_end = LinearColorMapper(palette=palette, low=0, high=300, nan_color='#d9d9d9')
+color_mapper_average_rating = LinearColorMapper(palette=palette, low=0, high=5, nan_color='#d9d9d9')
 
 # Create color bar.
-tick_labels = {'0': '0','50':'50', '100':'100', '150':'150', '200':'200', '250':'250', '300':'300+'}
-color_bar = ColorBar(color_mapper=color_mapper, label_standoff=8, width=500, height=20,
-                     border_line_color=None, location=(0, 0), orientation='horizontal', major_label_overrides=tick_labels)
+tick_labels = {'0': '0', '50': '50', '100': '100', '150': '150', '200': '200', '250': '250', '300': '300+'}
+color_bar_start = ColorBar(color_mapper=color_mapper_start, label_standoff=8, width=500, height=20,
+                     border_line_color=None, location=(0, 0), orientation='horizontal')
+color_bar_end = ColorBar(color_mapper=color_mapper_end, label_standoff=8, width=500, height=20,
+                     border_line_color=None, location=(0, 0), orientation='horizontal',
+                     major_label_overrides=tick_labels)
+color_bar_average_rating = ColorBar(color_mapper=color_mapper_average_rating, label_standoff=8, width=500, height=20,
+                     border_line_color=None, location=(0, 0), orientation='horizontal')
 
 # Create figure object.
-geo_map = figure(title='Stats per Country', plot_height=600, plot_width=950, toolbar_location=None)
-geo_map.xgrid.grid_line_color = None
-geo_map.ygrid.grid_line_color = None
+geo_map_start = figure(title='Daily Sales at 2021-06-01', plot_height=400, plot_width=750, toolbar_location=None)
+geo_map_end = figure(title='Sales over Time at 2021-12-31', plot_height=400, plot_width=750, toolbar_location=None)
+geo_map_average_rating = figure(title='Total Average Rating per Country', plot_height=400, plot_width=750, toolbar_location=None)
+geo_map_start.xgrid.grid_line_color = None
+geo_map_start.ygrid.grid_line_color = None
+geo_map_end.xgrid.grid_line_color = None
+geo_map_end.ygrid.grid_line_color = None
+geo_map_average_rating.xgrid.grid_line_color = None
+geo_map_average_rating.ygrid.grid_line_color = None
 
 # Add patch renderer to figure.
-#geo_map_patches = geo_map.patches('xs', 'ys', source=geosource_countries,
-#                                  fill_color={'field': 'TotalAverageRating', 'transform': color_mapper},
-#                                  line_color='black', line_width=0.25, fill_alpha=1)
-#geo_map_patches2 = geo_map.patches('xs', 'ys', source=geosource_countries,
-#                                  fill_color={'field': 'DailySales', 'transform': color_mapper},
-#                                  line_color='black', line_width=0.25, fill_alpha=1)
-geo_map_patches = geo_map.patches('xs', 'ys', source=geosource_countries,
-                                fill_color={'field': 'SalesOverTime', 'transform': color_mapper},
-                                line_color='black', line_width=0.25, fill_alpha=1)
+geo_map_patches_start = geo_map_start.patches('xs', 'ys', source=geosource_countries_start,
+                                  fill_color={'field': 'DailySales', 'transform': color_mapper_start},
+                                  line_color='black', line_width=0.25, fill_alpha=1)
+geo_map_patches_end = geo_map_end.patches('xs', 'ys', source=geosource_countries_end,
+                                  fill_color={'field': 'SalesOverTime', 'transform': color_mapper_end},
+                                  line_color='black', line_width=0.25, fill_alpha=1)
+geo_map_patches_average_rating = geo_map_average_rating.patches('xs', 'ys', source=geosource_countries_end,
+                                  fill_color={'field': 'TotalAverageRating', 'transform': color_mapper_average_rating},
+                                  line_color='black', line_width=0.25, fill_alpha=1)
+
 # Specify figure layout.
-geo_map.add_layout(color_bar, 'below')
+geo_map_start.add_layout(color_bar_start, 'below')
+geo_map_end.add_layout(color_bar_end, 'below')
+geo_map_average_rating.add_layout(color_bar_average_rating, 'below')
 
 # Create hover tool
-geo_map.add_tools(HoverTool(renderers=[geo_map_patches],
+geo_map_start.add_tools(HoverTool(renderers=[geo_map_patches_start],
                             tooltips=[('Country', '@country'),
-                                      ('Total Average Rating', '@TotalAverageRating'), ('Sales over Time', '@SalesOverTime'),
+                                      ('Total Average Rating', '@TotalAverageRating'),
+                                      ('Sales over Time', '@SalesOverTime'),
                                       ('Daily Sales', '@DailySales')]))
+geo_map_end.add_tools(HoverTool(renderers=[geo_map_patches_end],
+                            tooltips=[('Country', '@country'),
+                                      ('Total Average Rating', '@TotalAverageRating'),
+                                      ('Sales over Time', '@SalesOverTime'),
+                                      ('Daily Sales', '@DailySales')]))
+geo_map_average_rating.add_tools(HoverTool(renderers=[geo_map_patches_average_rating],
+                            tooltips=[('Country', '@country'),
+                                      ('Total Average Rating', '@TotalAverageRating')]))
+
+# Set up geomap layout
+geo_layout = gridplot([[geo_map_start, geo_map_end], [geo_map_average_rating, None]])
 
 
-# Make a slider object
-def update_map(attr, old, new):
-    yr = slider.value
-    geosource_countries.geojson = set_date(yr)
 
+#Making the KPI figure
 
-slider = DateSlider(title='Day', start=datetime.date(2021, 6, 1), end=datetime.date(2021, 12, 31),
-                    value=datetime.date(2021, 6, 1))
-slider.on_change('value', update_map)
+#Data setup
+data_ratings.rename(columns={"Daily Average Rating": "DailyAverageRating"}, inplace=True)
+data_crashes.rename(columns={"Daily Crashes": "DailyCrashes"}, inplace=True)
 
-# Adding buttons to choose which data to show
-#def radio_button_date():
-#    if radio_button_group.active == "Total Average Rating":
-#        geo_map.renderers=geo_map_patches
-#    elif radio_button_group.active == "Daily Sales":
-#        geo_map.renderers=geo_map_patches2
-#    elif radio_button_group.active == "Sales over Time":
-#        geo_map.renderers=geo_map_patches3
+merged_ratings_crashes = data_crashes.merge(data_ratings, left_on="Date", right_on="Date",
+                       how="left")
+merged_ratings_crashes = merged_ratings_crashes.dropna(subset=['DailyAverageRating', 'DailyCrashes'])
+ratings_crashes_source = ColumnDataSource(merged_ratings_crashes)
 
+#Prepare data for linear regression
+x=merged_ratings_crashes["DailyCrashes"].to_numpy()
+y=merged_ratings_crashes["DailyAverageRating"].to_numpy()
 
-#LABELS = ["Total Average Rating", "Daily Sales", "Sales over Time"]
+#Create linear regression model using scikit learn
+linear_model = LinearRegression().fit(x.reshape(-1, 1), y)
 
-#radio_button_group = RadioButtonGroup(labels=LABELS, active=0)
-#radio_button_group.on_click(radio_button_date)
+#Get the slope and intercept from the model
+slope = linear_model.coef_[0]
+intercept = linear_model.intercept_
 
-# Add the slider to the figure
-geo_layout = column(slider, geo_map)
+#Make the trend line
+trend_line = Slope(gradient=slope, y_intercept=intercept,line_color='black')
+
+#Create the tools for the figure
+select_tools = ['box_select', 'lasso_select', 'poly_select', 'tap', 'reset']
+
+#Creating the figure
+number_of_crashes_vs_ratings = figure(plot_height=500,
+                                      plot_width=500,
+                                      x_axis_label='Daily Crashes',
+                                      y_axis_label='Daily Average Rating',
+                                      title='Number of Crashes vs Daily Average Rating, 2021-06 - 2021-12',
+                                      toolbar_location='below',
+                                      tools=select_tools)
+
+#Add the data points to the figure
+number_of_crashes_vs_ratings.square(x='DailyCrashes',
+                                    y='DailyAverageRating',
+                                    source=ratings_crashes_source,
+                                    color='royalblue',
+                                    selection_color='deepskyblue',
+                                    nonselection_color='lightgray',
+                                    nonselection_alpha=0.3)
+
+#Making a hover renderer
+hover_circles = number_of_crashes_vs_ratings.circle(x='DailyCrashes', y='DailyAverageRating',
+                                                    source=ratings_crashes_source,
+                                                    size=15, alpha=0,
+                                                    hover_fill_color='black', hover_alpha=0.5)
+
+#Add the trend line
+number_of_crashes_vs_ratings.add_layout(trend_line)
+
+#Create tooltips for the hover tool
+tooltips = [
+    ('Date', '@Date'),
+    ('Daily Average Rating', '@DailyAverageRating'),
+    ('Daily Crashes', '@DailyCrashes')
+]
+
+number_of_crashes_vs_ratings.add_tools(HoverTool(tooltips=tooltips, renderers=[hover_circles]))
+kpi_figure = column(number_of_crashes_vs_ratings)
 
 # Generate the HTML
-p = column(fig3, geo_layout)
+p = column(fig3, kpi_figure, geo_layout)
 show(p)
